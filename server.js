4fd50b0ab469874d2f842cbc941df8d5eb2ea27c
nishadinfo09 <mdnishadali03@gmail.com>
2026-02-09 17:09:01 +0600

    setup the project

---
+++ server.js
@@ -0,0 +1,13 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
