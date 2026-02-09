14d9ec41a51d35f36ed5afdc9a19ed4b78bf7f20
nishadinfo09 <mdnishadali03@gmail.com>
2026-02-09 18:17:44 +0600

    add login controller also generate access and refresh Token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,43 @@
 import { UserModel } from "../models/user.model";
+import jwt from "jsonwebtoken";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = UserModel.findById(userId);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "user doesn't exist" });
+    }
+    const accessToken = await user.generateAccessToken();
+    if (!accessToken) {
+      return res
+        .status(400)
+        .json({ success: false, message: "accessToken doesn't exist" });
+    }
+    const refreshToken = await user.generateRefreshToken();
+    if (!refreshToken) {
+      return res
+        .status(400)
+        .json({ success: false, message: "refreshToken doesn't exist" });
+    }
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.log(
+      "Something went wrong while generating referesh and access token",
+    );
+  }
+};
+
+const options = {
+  httpOnly: true,
+  secure: true,
+};
 
 const regieterUser = async (req, res) => {
   try {
@@ -56,4 +95,53 @@ const regieterUser = async (req, res) => {
       .json({ success: false, message: "Internal server error" });
   }
 };
-export { regieterUser };
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "email and password not exist" });
+    }
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "user doesn't exist" });
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+      throw new ApiError(401, "Invalid user credentials");
+    }
+
+    const { accessToken, refreshToken } = generateAccessAndRefreshToken(
+      user._id,
+    );
+
+    const loggedInUser = await UserModel.findById(user._id).select(
+      "-password -refreshToken",
+    );
+
+    return res
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .status(200)
+      .json({
+        success: true,
+        message: "user login successfully",
+        user: loggedInUser,
+        accessToken,
+        refreshToken,
+      });
+  } catch (error) {
+    console.log("login controller error", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+export { regieterUser, loginUser };
