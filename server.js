b0ff6aeea7c4b5f9214c154a0a12ad687974e361
nishadinfo09 <mdnishadali03@gmail.com>
2026-02-09 19:59:51 +0600

    remove port and listen for production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./src/lib/connectDB.js";
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,6 +13,6 @@ connectDB()
   .then(() => console.log("Database Connected Successfully"))
   .catch((error) => console.log("Database connection failed", error));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
