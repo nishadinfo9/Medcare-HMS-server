547c864f6f785f3df5e5cc73d14fa0d296653bac
nishadinfo09 <mdnishadali03@gmail.com>
2026-02-11 21:25:30 +0600

    update cors middleware to use environment variable for origin

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  }),
+);
 
 import userRouter from "./src/routes/user.route.js";
 app.use("/api/v1", userRouter);
